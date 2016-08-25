85960d257a6d09b58f71c2f5d2fc52d9d35b7d36
nacker <648959@qq.com>
2016-08-25 16:00:15 +0800

    project init

---
+++ Assets/HoloTest.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HoloTest : MonoBehaviour {
+
+
+// Use this for initialization
+    void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(0,2,0);
+    }
+
+    void OnSelect()
+    {
+
+    }
+}
